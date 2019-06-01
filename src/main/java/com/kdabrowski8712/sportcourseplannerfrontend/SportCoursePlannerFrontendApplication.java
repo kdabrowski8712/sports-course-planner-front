b54c4ffe28f9529e7d6a598246242dd1c5acffcf
k.dabrowski8712 <k.dabrowski8712@gmail.com>
2019-06-01 11:43:25 +0200

    initial commit

---
+++ src/main/java/com/kdabrowski8712/sportcourseplannerfrontend/SportCoursePlannerFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kdabrowski8712.sportcourseplannerfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportCoursePlannerFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportCoursePlannerFrontendApplication.class, args);
+    }
+
+}
